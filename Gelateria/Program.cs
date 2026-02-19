018e810d9259ef2db6dfd4917e7e677e96dcd479
Sothebaebys <176383400+Sothebaebys@users.noreply.github.com>
2026-02-19 17:24:25 +0100

    Update Program.cs

--- Gelateria/Program.cs
+++ Gelateria/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Gelateria
 {
-// void NomeFunzioneMetodo()
-// {
 
-// } 
-static void Main(string [] args)
-{
+   public static void StampaMenu(string[] gusti, double[] prezzi, int[] indice)
+   { 
+      Console.WriteLine("Gusti:");
 
-}
+      for(int i=0; i < indice.Length; i++)
+      {
+         Console.WriteLine($"    -{gusti[i]}\n                 Prezzo:  €{prezzi[i]}");
+         Console.WriteLine("--------------------------------------------------");
+      }
+   }
+   
+   static void Main(string [] args)
+   {
+   StampaMenu(["Pippo","pluto","paperino"],[12.5,5.1,3.3],[1,2,3]);
+   }
 }
