db2e5000dd138f9e2c9b6957b6aa4e4f817bc420
fcard-dev <fabrizia.cardinale@gmail.com>
2026-02-19 17:32:27 +0100

    inizio Main gelateria
    bozza main gelateria

--- Gelateria/Program.cs
+++ Gelateria/Program.cs
@@ -2,12 +2,32 @@
 
 class Gelateria
 {
-// void NomeFunzioneMetodo()
-// {
+    string[] gusti = new { "cioccolato", "vaniglia", "fragola", "pistacchio", "limone" };
+    double[] prezzi = new { 1.50, 1.20, 1.30, 1.60, 1.10 };
 
-// } 
-static void Main(string [] args)
-{
+    static void Main(string[] args)
+    {
+        bool continua = true;
+        string selezione = "";
 
+        while (continua)
+        {
+            //StampaMenu();
+            Console.WriteLine("Inserisci l'indice del gusto: ");
+            int sceltaGusto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci numero palline: ");
+            int sceltaPalline = int.Parse(Console.ReadLine);
+
+            Console.WriteLine("Vuoi aggiungerne un altro gusto? S/N: ");
+            if (Console.ReadLine() != "S")
+            {
+                continua = false;
+                Console.WriteLine("Ordine terminato.");
+               //CalcolaTotale();
+            }
+
+        }
+
+    }
 }
-}
+
