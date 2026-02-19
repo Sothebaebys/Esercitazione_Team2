88067fd9ebcf2d0fd376c13399d572cbacdf1e19
4ndr3x-osu <andreap2007@proton.me>
2026-02-19 17:41:52 +0100

    Creazione metodo CalcoloTotale
    Metodo CalcoloTotale con metodo main per debug

--- Gelateria/Program.cs
+++ Gelateria/Program.cs
@@ -2,12 +2,33 @@
 
 class Gelateria
 {
-// void NomeFunzioneMetodo()
-// {
-
-// } 
-static void Main(string [] args)
-{
-
-}
+    static double CalcoloTotale(double[] prezzo, string quan, string sel, bool subtot)
+    {
+        double totale = 0;
+        string[] selezioni = sel.Split(" ");
+        string[] quantita = quan.Split(" ");
+        int giro = 0;
+        foreach(string s in selezioni)
+        {
+            totale += prezzo[(int.Parse(s)) - 1] * double.Parse(quantita[giro]);
+            //Console.WriteLine($"Gusto: {prezzo[int.Parse(s) - 1]} --- Quantita: {quantita[giro]}"); --- DEBUG
+            giro++;
+        }
+        if(totale > 10.0d && !subtot)
+        {
+            totale *= 0.9;
+        }
+        return totale;
+    }
+    /* static void Main(string [] args)
+    {
+        string[] gusti = new string[]{ "cioccolato", "vaniglia", "fragola", "pistacchio", "limone" };
+        double[] prezzi = new double[]{ 1.50, 1.20, 1.30, 1.60, 1.10 };
+        string selezione = "1 2 3 4 5";
+        string quantita = "2 2 3 2 1";
+        bool calcolosubtotale = true;
+        Console.WriteLine($"Il subtotale è: {CalcoloTotale(prezzi, quantita, selezione, calcolosubtotale)}");
+        calcolosubtotale = false;
+        Console.WriteLine($"Il totale è: {CalcoloTotale(prezzi, quantita, selezione, calcolosubtotale)}");
+    } */
 }
