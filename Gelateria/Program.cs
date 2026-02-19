98c87f570aa0ce8fc972b860d152622ddfad5416
4ndr3x-osu <andreap2007@proton.me>
2026-02-19 18:17:20 +0100

    Aggiunta commenti
    Aggiunta commenti per spiegare come funziona il metodo.

--- Gelateria/Program.cs
+++ Gelateria/Program.cs
@@ -11,7 +11,7 @@ class Gelateria
         foreach(string s in selezioni)
         {
             totale += prezzo[(int.Parse(s)) - 1] * double.Parse(quantita[giro]);
-            //Console.WriteLine($"Gusto: {prezzo[int.Parse(s) - 1]} --- Quantita: {quantita[giro]}"); --- DEBUG
+            //Console.WriteLine($"Gusto: {prezzo[int.Parse(s) - 1]} --- Quantita: {quantita[giro]}"); //--- DEBUG
             giro++;
         }
         if(totale > 10.0d && !subtot)
@@ -20,6 +20,21 @@ class Gelateria
         }
         return totale;
     }
+
+
+        //  NEL MAIN CAMBIARE LE SCELTE DI GUSTO E QUANTITA DA INT A STRING, PER FARLO FUNZIONARE CON LA FUNZIONE
+        //  DATO CHE LA FUNZIONE FA QUESTO:
+        //  SPLITTA ENTRAMBE LE STRINGHE E LE METTE IN UN'ARRAY
+        //  IL FOREACH SCORRE L'ARRAY DEL PREZZO BASANDOSI SUGLI INDICI SELEZIONATI DALL'UTENTE (es. IN CASO "SELEZIONE"
+        //  NEL MAIN HA COME ORDINE "2 2 5 4 1" NEL FOREACH I PREZZI CHE VERRANNO USATI SARANNO "1.20 - 1. 20 - 1.10 - 1.60 - 1.50").
+
+        //  UN ESEMPIO CHE POTREBBE FUNZIONARE E' string scelte = (Console.ReadLine() + " "), DATO CHE SERVE UN CARATTERE PER SPLITTARE LA STRINGA.
+        //  STESSA COSA PER LA QUANTITA'
+
+
+
+
+
     /* static void Main(string [] args)
     {
         string[] gusti = new string[]{ "cioccolato", "vaniglia", "fragola", "pistacchio", "limone" };
@@ -30,5 +45,5 @@ class Gelateria
         Console.WriteLine($"Il subtotale è: {CalcoloTotale(prezzi, quantita, selezione, calcolosubtotale)}");
         calcolosubtotale = false;
         Console.WriteLine($"Il totale è: {CalcoloTotale(prezzi, quantita, selezione, calcolosubtotale)}");
-    } */
+    } //*/ 
 }
